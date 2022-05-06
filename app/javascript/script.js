f236aa065a58ca28fc0f2401d04c50ab2b32d476
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-06 15:23:46 +0000

    [Add] JS-13

---
+++ app/javascript/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+  $("#theTarget").skippr({
+    transition : 'slide',
+    speed : 1000,
+    easing : 'easeOutQuart',
+    navType : 'block',
+    childrenElementType : 'div',
+    arrows : true,
+    autoPlay : true,
+    autoPlayDuration : 3000,
+    keyboardOnAlways : true,
+    hidePrevious : false
+  });
+});
