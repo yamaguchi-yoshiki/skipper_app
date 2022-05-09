e4e28c0ded4a8f649f33cad5a8f69b5489c421ce
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-09 07:58:15 +0000

    [Edit] chartkick test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,7 @@
 class TestsController < ApplicationController
   def top
+    @chartlabels = (0..6).map { |n| n.day.ago.strftime("%-m月 %-d日") }.reverse
+    @chartdatas = [10, 6, 3, 7, 4, 12, 2]
+    @chart1 = [@chartlabels, @chartdatas].transpose
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'jquery'
 import "skippr.min"
+import "chartkick/chart.js"
 import "script.js"
 
 Rails.start()
